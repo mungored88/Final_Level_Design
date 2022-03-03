2ff189c90ad76d223b424ff77e8e510bfc37dfb1
gonzalo.morenop <gonzalo.morenop@davinci.edu.ar>
2022-03-03 17:57:51 -0300

    fogOfWarScript

---
+++ Assets/Scripts/fogOfWarTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fogOfWarTrigger : MonoBehaviour
+{
+    MeshRenderer mesh;
+    private void Start()
+    {
+        mesh = this.GetComponent<MeshRenderer>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player")) this.mesh.enabled = false;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) this.mesh.enabled = true;
+    }
+}
